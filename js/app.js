c14c59e7d6405093f34fe58fe2720b7042cee77e
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-18 16:28:45 +0200

    presa parola dall'utente, divisa in un array, invertita e unita, se parola invertita = parola iniziale allora stampo true

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// Creo una funzione in cui viene inserita la parola digitata dall'utente
+// SE la parola è palindroma stampo "true"
+// ALTRIMENTI stampo "false"
+
+function isPalindrome(string) {
+    const parolaReverse = string.split("").reverse().join("")
+    if (parolaReverse === string) {
+        return true
+    } else {
+        return false
+    }
+}
+
 const parola = prompt("Inserisci una parola")
+console.log(isPalindrome(parola))
+
