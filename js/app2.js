8947f2f0e02557bd98727402c9b30cbb2698eed9
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-18 16:45:02 +0200

    iniziato  esercizio 2

---
+++ js/app2.js
@@ -0,0 +1,6 @@
+// Prendo il valore "pari"  o "dispari" dall'utente
+// Prendo un numero da 1 a 5 dall'utente
+// Genero un numero random
+// Sommo i 2 numeri
+// Stabilisco se la somma è  pari o dispari
+// Se il valore della somma corrisponde a ciò che  ha inserito l'utente, questo ha vinto, altrimenti ha perso
